f2f0947b7b3b73b5bcd4b0e9b33c9ecf699bd276
Remo Koch <me@rko.io>
2013-04-21 15:13:28 +0200

    Some formatting updates

--- Task_D/src/PriorityQueue.cpp
+++ Task_D/src/PriorityQueue.cpp
@@ -1,72 +1,59 @@
 /*
- Assignment 4 - Algorithms and data structures
- 
- University of Zurich
- Department of Informatics
- Vizualization and Multimedia Laboratory
- 
- Author: Genc Mazlami
+ * Assignment 4 - Algorithms and data structures
+ *
+ * University of Zurich
+ * Department of Informatics
+ * Vizualization and Multimedia Laboratory
+ *
+ * Author: Genc Mazlami
+ *
+ * Authored by Remo Koch
+ * Public repository at https://github.com/rkoch/uzh-inf02b-a4
  */
 
 #include <iostream>
 #include "PriorityQueue.h"
 
-
 //MISSING INCLUDE!! find the library to include...
 
+PriorityQueue::PriorityQueue() {
 
-PriorityQueue::PriorityQueue(){
-    
-    //create queue and fill it with starting elements
-    queue.push_back(99);
-    queue.push_back(3);
-    queue.push_back(45);
-    queue.push_back(2);
-    queue.push_back(13);
-    
-    
-    std::make_heap(queue.begin(), queue.end());   // build heap order on the queue, so that greatest element is on top
-    
-    
+	//create queue and fill it with starting elements
+	queue.push_back(99);
+	queue.push_back(3);
+	queue.push_back(45);
+	queue.push_back(2);
+	queue.push_back(13);
+
+	std::make_heap(queue.begin(), queue.end());          // build heap order on the queue, so that greatest element is on top
+
 }
 
+int PriorityQueue::getFrontElement() {          //return the topelement
 
+	return queue.at(0);
 
-
-
-int PriorityQueue::getFrontElement(){  //return the topelement
-    
-    return queue.at(0);
-    
 }
 
+void PriorityQueue::insert(int _element) {
 
-
-void PriorityQueue::insert(int _element){
-    
-
-    //TODO: insert code here
+	//TODO: insert code here
 }
 
+void PriorityQueue::remove() {
 
-void PriorityQueue::remove(){
-    
-    //TODO: insert code here
+	//TODO: insert code here
 }
 
-
-void PriorityQueue::heapsort(){
-    //TODO: insert code here    
+void PriorityQueue::heapsort() {
+	//TODO: insert code here
 }
 
-void PriorityQueue::printQueue(){
-    std::cout << "Current Queue: " << std::endl;
-    
-   
-    //TODO: insert code here
-    
-    
-    std::cout << std::endl;
-}
+void PriorityQueue::printQueue() {
+	std::cout << "Current Queue: " << std::endl;
 
+	//TODO: insert code here
+
+	std::cout << std::endl;
+}
 
--- Task_D/src/PriorityQueue.h
+++ Task_D/src/PriorityQueue.h
@@ -1,12 +1,14 @@
-
 /*
- Assignment 4 - Algorithms and data structures
- 
- University of Zurich
- Department of Informatics
- Vizualization and Multimedia Laboratory
- 
- Author: Genc Mazlami
+ * Assignment 4 - Algorithms and data structures
+ *
+ * University of Zurich
+ * Department of Informatics
+ * Vizualization and Multimedia Laboratory
+ *
+ * Author: Genc Mazlami
+ *
+ * Authored by Remo Koch
+ * Public repository at https://github.com/rkoch/uzh-inf02b-a4
  */
 
 #ifndef heaps_priority_queues_PriorityQueue_h
@@ -14,32 +16,25 @@
 
 #include <vector>
 
-
-
 class PriorityQueue {
-    
-    
-private:
-    std::vector<int>  queue;
-    
-    
-    
-    
-public:
-    
-    
-    PriorityQueue();
-    
-    int getFrontElement();
-    
-    void insert(int _element);
-    
-    void remove();
-    
-    void heapsort();
-    
-    void printQueue();
-};
 
+private:
+	std::vector<int> queue;
+
+public:
+
+	PriorityQueue();
+
+	int getFrontElement();
+
+	void insert(int _element);
+
+	void remove();
+
+	void heapsort();
+
+	void printQueue();
+
+};
 
 #endif
